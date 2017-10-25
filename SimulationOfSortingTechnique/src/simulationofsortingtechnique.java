9f23f80ea27efe47020ea30e2a26741a0ac5245d
Anisha Agarwal <anisha9000@gmail.com>
2017-10-24 19:38:50 -0700

    calling each sorting 5 times to find mean
    Signed-off-by: Anisha Agarwal <anisha9000@gmail.com>

--- SimulationOfSortingTechnique/src/simulationofsortingtechnique.java
+++ SimulationOfSortingTechnique/src/simulationofsortingtechnique.java
@@ -12,6 +12,13 @@ public class simulationofsortingtechnique {
 
     static final long MEGABYTE = 1024L * 1024L;
 
+    //Object creation
+    static MergeSort mergeSort = new MergeSort();
+    static QuickSort quickkSort = new QuickSort();
+    static SelectionSort selectSort = new SelectionSort();
+    static BubbleSort bubbleSort = new BubbleSort();
+    static InsertionSort insertionSort = new InsertionSort();
+
     /**
      * @param args the command line arguments
      */
@@ -19,91 +26,81 @@ public class simulationofsortingtechnique {
 
         for (int iteration = 0; iteration < 5; iteration++) {
             double array[] = generateData(0);
+            long time1 = java.lang.System.currentTimeMillis();
+
+            for (int sortType = 1; sortType <= 5; sortType++) {
+                computeSorting(array, sortType);
+            }
+
         }
-
-        /* Generating data set containing uniformly distributed random Double values using ThreadLocalRandom*/
- /*usage: genRandom(no. of datapoints, min value, max value)*/
- /*usage: genGaussian(no. of datapoints, mean value, variance value)*/
-        
-        double arr2[] = generateData(0);
-        double[] array_insertion = arr2.clone();
-        double[] array_bubble = arr2.clone();
-        double[] array_merge = arr2.clone();
-
-        double[] array_selection = arr2.clone();
-
-        MergeSort mergeSort = new MergeSort();
-        QuickSort quickkSort = new QuickSort();
-        SelectionSort selectSort = new SelectionSort();
-        BubbleSort bubbleSort = new BubbleSort();
-        InsertionSort insertionSort = new InsertionSort();
-
-        //Merge sort 
-        System.out.println("Merge sort");
-        Runtime runtime = Runtime.getRuntime();
-        runtime.gc();
-        double memoryInit = (double) (runtime.totalMemory() - runtime.freeMemory());
-
-        mergeSort.sort(array_merge, 0, array_merge.length - 1);
-
-        double memoryFinal = (double) (runtime.totalMemory() - runtime.freeMemory());
-        System.out.println((double) (memoryFinal - memoryInit) / MEGABYTE);
-
-        System.out.println("Quick sort");
-        memoryInit = (double) (runtime.totalMemory() - runtime.freeMemory());
-        double[] array_quick = arr2.clone();
-        quickkSort.sort(array_quick, 0, array_quick.length - 1);
-
-        memoryFinal = (double) (runtime.totalMemory() - runtime.freeMemory());
-        System.out.println((double) (memoryFinal - memoryInit) / MEGABYTE);
-
-        runtime.gc();
-        System.out.println("Selection sort");
-        memoryInit = (double) (runtime.totalMemory() - runtime.freeMemory());
-
-        selectSort.sort(array_selection);
-
-        memoryFinal = (double) (runtime.totalMemory() - runtime.freeMemory());
-        System.out.println((double) (memoryFinal - memoryInit) / MEGABYTE);
-
-        runtime.gc();
-        System.out.println("Bubble sort");
-        memoryInit = (double) (runtime.totalMemory() - runtime.freeMemory());
-
-        bubbleSort.sort(array_bubble);
-
-        memoryFinal = (double) (runtime.totalMemory() - runtime.freeMemory());
-        System.out.println((double) (memoryFinal - memoryInit) / MEGABYTE);
-
-        runtime.gc();
-        System.out.println("Insertion sort");
-        memoryInit = (double) (runtime.totalMemory() - runtime.freeMemory());
-
-        insertionSort.sort(array_insertion);
-
-        memoryFinal = (double) (runtime.totalMemory() - runtime.freeMemory());
-        System.out.println((double) (memoryFinal - memoryInit) / MEGABYTE);
 
     }
 
     private static double[] generateData(int i) {
+        /* Generating data set containing uniformly distributed random Double values using ThreadLocalRandom*/
+
         GenSynDataSet data = new GenSynDataSet();
         ReadInput realInput = new ReadInput();
-        double[] arr2 = null;     
+        double[] arr2 = null;
         switch (i) {
             case 0:
+                /*usage: genRandom(no. of datapoints, min value, max value)*/
                 arr2 = data.genRandom(50000, 1, 1000);
                 return arr2;
             case 1:
+                /*usage: genGaussian(no. of datapoints, mean value, variance value)*/
                 arr2 = data.genGaussian(50000, 100.0, 5.0);
                 return arr2;
-            case 2: 
+            case 2:
                 arr2 = realInput.readListingFile();
                 return arr2;
-                //TODO
+            //TODO
             //case 3:
         }
         return arr2;
     }
 
+    private static long getMemory(Runtime runtime) {
+        return runtime.totalMemory() - runtime.freeMemory();
+    }
+
+    private static void computeSorting(double[] array, int sortType) {
+        long startTime = System.currentTimeMillis();
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        long startMemory = getMemory(runtime);
+        String sortTypeStr = null;
+        double[] array_temp = array.clone();
+
+        switch (sortType) {
+            case 1:
+                //Bubble Sort
+                sortTypeStr = "Bubble";
+                bubbleSort.sort(array_temp);
+                break;
+            case 2:
+                //Merge Sort
+                sortTypeStr = "Merge";
+                mergeSort.sort(array_temp, 0, array_temp.length - 1);
+                break;
+            case 3:
+                // Quick Sort
+                sortTypeStr = "Quick";
+                quickkSort.sort(array_temp, 0, array_temp.length - 1);
+                break;
+            case 4:
+                // Selection Sort
+                sortTypeStr = "Selection";
+                selectSort.sort(array_temp);
+                break;
+            case 5:
+                //Insertion Sort
+                sortTypeStr = "Insertion";
+                insertionSort.sort(array_temp);
+                break;
+        }
+
+        long endMeomry = getMemory(runtime);
+        long endTime = System.currentTimeMillis();
+    }
 }
